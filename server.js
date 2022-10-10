fb6cde34c4ff5e6063b8e84faf02f1140f862af4
sinhaugoh <sinhaugoh456@hotmail.com>
2022-10-10 17:45:26 +0800

    initial authors routes

--- server.js
+++ server.js
@@ -6,8 +6,10 @@ if (process.env.NODE_ENV !== "production") {
 import path from "path";
 import { fileURLToPath } from "url";
 import { router as indexRouter } from "./routes/index.js";
+import { router as authorRouter } from "./routes/authors.js";
 import express from "express";
 import expressLayouts from "express-ejs-layouts";
+import bodyParser from "body-parser";
 import mongoose from "mongoose";
 
 const __filename = fileURLToPath(import.meta.url);
@@ -20,6 +22,7 @@ app.set("views", __dirname + "/views");
 app.set("layout", "layouts/layout");
 app.use(expressLayouts);
 app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ limit: "10mb", extended: false }));
 
 mongoose.connect(process.env.DATABASE_URL, {
   useNewUrlParser: true,
@@ -30,5 +33,6 @@ db.on("error", (e) => console.error(e));
 db.once("open", () => console.log("Connected to Mongoose"));
 
 app.use("/", indexRouter);
+app.use("/authors", authorRouter);
 
 app.listen(process.env.PORT || 3000);
